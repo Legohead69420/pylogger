1a5309033ab9e10e7e052881f0b6c93dfc45746b
Legohead69420 <106709580+Legohead69420@users.noreply.github.com>
2023-12-20 18:12:52 -0500

    Update log_installer.py

--- log_installer.py
+++ log_installer.py
@@ -87,7 +87,7 @@ if version=="":
     response = wikipedia.page(install_v2_1)
     rc = str(response.content)
     rc = rc.replace(' ', ' ')
-    file = f'C:/Users/{getpass.getuser()}/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\pylogger\__init__.pyi'
+    file = f'C:/Users/{getpass.getuser()}/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\pylogger\__init__.py'
     with open(file, "w") as install:
         install.write(rc)
     if log=="yes":
