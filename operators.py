0b393c7129d1e18fad2c7bd8bf5750201d7aa3f5
Legohead69420 <106709580+Legohead69420@users.noreply.github.com>
2023-12-30 10:22:45 -0500

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,53 @@
+import os
+try:
+    from numba import njit
+except ImportError:
+    os.system('pip install numba')
+    from numba import njit
+cls = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+class lo:
+    '''
+    Looping mathematical operations
+    '''
+    def find_multiplier(start: float, toi: float, outputstr: bool) -> float:
+        '''
+        Starts from inputted value `start` and returns multiplier to arrive to `toi` from `start`
+        '''
+        mult = 0
+        out = 0
+        while not out >= toi:
+            mult += 0.01
+            out = start * mult
+        offset = start * mult - toi
+        ostr = f'Multiplier to reach {toi} from {start} is about {mult}.\nOffset is {offset}.\nExact multiplier is impossible to find due to INSANE wait time'
+        if outputstr:
+            return ostr
+        else:
+            return mult
+
+class o():
+    '''
+    Mathematical operations
+    '''
+    def tan(x: float) -> float:
+        import math
+        return math.tan(x)
+
+    def pi(digits: int) -> float:
+        import os
+        try:
+            from mpmath import mp
+        except ImportError:
+            os.system('pip install mpmath')
+
+        mp.dps = digits
+        return mp.pi
+    
+    def pow(y1: float, y2: float) -> float:
+        import math
+        return math.pow(y1, y2)
+
+cls()
+print(o.pi(50))
+exit()
+print(lo.find_multiplier(3.14159, 250, True))
